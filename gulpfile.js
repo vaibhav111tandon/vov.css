a01d70ab5246fb2a504f067d537e7a1945b7e5f6
vitebo <vitebo@hotmail.com>
2019-10-12 15:25:01 -0300

    add compile task
    Signed-off-by: vitebo <vitebo@hotmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-function defaultTask(cb) {
-  cb();
+const { src, dest } = require('gulp');
+const concat = require('gulp-concat');
+const csso = require('gulp-csso');
+const uglifycss = require('gulp-uglifycss');
+
+function compile() {
+  return src('css/**/*.css')
+    .pipe(concat('vov.min.css'))
+    .pipe(csso())
+    .pipe(uglifycss())
+    .pipe(dest('./'));
 }
 
-exports.default = defaultTask;
+exports.compile = compile;
