f4578d309d858c6157e2e092b3122d47dc922447
vitebo <vitebo@hotmail.com>
2019-10-12 15:34:39 -0300

    add gulp-autoprefixer plugin
    Signed-off-by: vitebo <vitebo@hotmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const { src, dest } = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const csso = require('gulp-csso');
 const uglifycss = require('gulp-uglifycss');
@@ -6,6 +7,7 @@ const uglifycss = require('gulp-uglifycss');
 function compile() {
   return src('css/**/*.css')
     .pipe(concat('vov.min.css'))
+    .pipe(autoprefixer())
     .pipe(csso())
     .pipe(uglifycss())
     .pipe(dest('./'));
